7f25aba3e4a6e905086a7ff70aa287dd206bc50c
LukeZHar <luke.z.harris@hotmail.com>
2025-06-17 23:52:42 +1000

    Feat: added out of order on message

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -233,7 +233,7 @@ export default function ContactPage() {
                       ) : (
                         <>
                           <Send className="mr-2 h-4 w-4" />
-                          Send Message
+                          Send Message (Out Of Order)
                         </>
                       )}
                     </Button>
